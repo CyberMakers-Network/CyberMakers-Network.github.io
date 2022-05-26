cda936e9e3a597bb98b3674de39c34eb2f34b368
Uemerson Pinheiro Junior <uemersonpinheirojunior@gmail.com>
2022-05-26 11:29:19 -0300

    feat: add components and pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import AppProvider from '../contexts';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <title>CyberMakers Network</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <AppProvider>
+        <Component {...pageProps} />
+      </AppProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
